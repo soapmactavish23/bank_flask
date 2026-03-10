e0403cbfc3a86474884e60bcb7f0f4b8fdcb82ba
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-09 21:01:01 -0300

    Implementando Ações no Banco de Dados - parte 2

--- src/models/repository/user_repository.py
+++ src/models/repository/user_repository.py
@@ -11,3 +11,21 @@ class UserRepository:
                 INSERT INTO users (username, password, balance) VALUES (?, ?, ?);
             ''',(username, password, 0))
         self.__conn.commit()
+
+    def edit_balance(self, user_id: int, new_balance: float) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            UPDATE users SET balance = ? WHERE id = ?;
+            ''', (new_balance, user_id)
+        )
+        self.__conn.commit()
+
+    def get_user_by_username(self, username: str) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            SELECT id, username, password FROM users WHERE username = ?;
+            ''', (username,)
+        )
+        return cursor.fetchone()
--- src/models/repository/user_repository_test.py
+++ src/models/repository/user_repository_test.py
@@ -2,12 +2,14 @@ from src.models.repository.user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
 
 
-def test_registry_user():
+def test_repository():
     db_connection_handler.connect()
     conn = db_connection_handler.get_connection()
     repo = UserRepository(conn)
 
-    username = "barney"
+    username = "Bob esponja"
     password = "123Rocket!"
 
-    repo.registry_user(username, password)
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)

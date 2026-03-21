15942d4d86fb71971f33e5028e3419759f6a5698
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-21 18:05:43 -0300

    Variáveis de Ambiente

--- src/drivers/jwt_handler.py
+++ src/drivers/jwt_handler.py
@@ -2,17 +2,19 @@ from datetime import datetime, timedelta, timezone
 import jwt
 from typing import Dict
 
+from src.configs.jwt_configs import jwt_infos
+
 
 class JwtHandler:
     def create_jwt_token(self, body: Dict = {}) -> str:
         return jwt.encode(
             payload={
-                'exp': datetime.now(timezone.utc) + timedelta(minutes=1),
+                'exp': datetime.now(timezone.utc) + timedelta(hours=int(jwt_infos['JWT_HOURS'])),
                 **body
             },
-            key='minhaChave',
-            algorithm='HS256'
+            key=jwt_infos['KEY'],
+            algorithm=jwt_infos['ALGORITHM']
         )
 
     def decode_jwt_token(self, token: str) -> Dict:
-        return jwt.decode(token, key='minhaChave', algorithms=['HS256'])
+        return jwt.decode(token, key=jwt_infos['KEY'], algorithms=[jwt_infos['ALGORITHM']])

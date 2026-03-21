c3ff39287db44f45cf157eae86c87e02e2bc6912
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-21 18:21:45 -0300

    Testando Lógica de Login de Usuário

---
+++ src/controllers/login_creator_test.py
@@ -0,0 +1,26 @@
+import pytest
+
+from src.controllers.login_creator import LoginCreator
+from src.drivers.password_handler import PasswordHandler
+
+username = "meuUsername"
+password = "minhaSenha"
+hashed_password = PasswordHandler().encrypt_password(password)
+
+class MockUserRepository:
+    def get_user_by_username(self, username: str):
+        return (10, username, hashed_password)
+
+def test_create():
+    login_creator = LoginCreator(MockUserRepository())
+    response = login_creator.create(username, password)
+
+    assert response["access"] == True
+    assert response["username"] == username
+    assert response["token"] is not None
+
+def test_create_with_wrong_password():
+    login_creator = LoginCreator(MockUserRepository())
+
+    with pytest.raises(Exception):
+        login_creator.create(username, "algumaSenha")

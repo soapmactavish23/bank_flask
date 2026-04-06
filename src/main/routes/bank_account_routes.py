93f8d4fe6d5f80a6d0bef7155f63812de381b6ce
soapmactavish23 <ricknogueira1231@gmail.com>
2026-04-06 16:02:47 -0300

    Rotas de Funcionalidades Bancárias

--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from src.main.composer.balance_editor_composer import balance_editor_composer
+from src.main.composer.user_register_composer import user_register_composer
+from src.views.http_types.http_request import HttpRequest
 
 bank_routes_bp = Blueprint("bank_routes", __name__)
 
-@bank_routes_bp.route("/", methods=["GET"])
-def hello():
-    return jsonify({"message": "Hello World!"}), 200
+@bank_routes_bp.route("/bank/registry", methods=["GET"])
+def registry_user():
+    http_request = HttpRequest(body=request.json)
+    http_response = user_register_composer().handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+@bank_routes_bp.route("/bank/login", methods=["POST"])
+def create_login():
+    http_request = HttpRequest(body=request.json)
+    http_response = user_register_composer().handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+@bank_routes_bp.route("/bank/balance/<user_id>", methods=["PATCH"])
+def edit_balance(user_id):
+    http_request = HttpRequest(body=request.json, params={'user_id': user_id})
+    http_response = balance_editor_composer().handle(http_request)
